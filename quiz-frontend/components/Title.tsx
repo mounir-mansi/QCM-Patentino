28a7923e57f0f48dd7c2641e675b9c0a80edd958
EmiDip <di.palma.emilie@outlook.fr>
2023-06-12 11:32:41 +0200

    taille titre

--- quiz-frontend/components/Title.tsx
+++ quiz-frontend/components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
     return (
         <div className="bg-gray-dark container w-[90%] my-10 ml-[5%] shadow-md strong rounded-md">
-            <div className='text-center uppercase text-4xl py-5 text-red-600 shadow'>Deviendras-tu le chef de la Quizine ?</div>
+            <div className='text-center uppercase sm:text-xl md:text-2xl lg:text-4xl py-5 text-red-600 shadow'>Deviendras-tu le chef de la Quizine ?</div>
         </div>
     );
 };
