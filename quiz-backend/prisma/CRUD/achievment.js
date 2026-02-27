ed14c02d2385f325723dc87a277f6225c4d13f09
mounir-mansi <mansimounir1@gmail.com>
2026-02-27 10:38:30 +0100

    refactor: instance PrismaClient unique partagée

--- quiz-backend/prisma/CRUD/achievment.js
+++ quiz-backend/prisma/CRUD/achievment.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../client.js";
 
 async function createAchievement(moduleId, userId, score, level, success) {
   try {
