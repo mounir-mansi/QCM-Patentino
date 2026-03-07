e663a23cb8ac08f2422ad42550bc3d135d09478f
mounir-mansi <mansimounir1@gmail.com>
2026-03-07 07:51:24 +0100

    style: fix homepage layout and responsive

--- quiz-frontend/components/Title.tsx
+++ quiz-frontend/components/Title.tsx
@@ -5,7 +5,7 @@ const Title = (props: any) => {
     return (
 
 
-        <div className="bg-gray-dark my-10 shadow-md strong rounded-md container">
+        <div className="bg-gray-dark my-2 mb-4 shadow-md strong rounded-md container">
             <div className="place-items-center text-center uppercase sm:text-xl md:text-2xl lg:text-4xl py-5 text-white font-outline-2 font-extrabold shadow">{props.title}</div>
         </div>
 
