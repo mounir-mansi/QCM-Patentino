6031c3ef7b924a47182be1b0f6f50dffa9ea8e81
mounir-mansi <mansimounir1@gmail.com>
2026-03-07 08:44:15 +0100

    style: fix navbar mobile and footer responsive

--- quiz-frontend/components/Footer.tsx
+++ quiz-frontend/components/Footer.tsx
@@ -1,37 +1,24 @@
-
-
-import {Footer} from "flowbite-react";
-
-
+import { Footer } from "flowbite-react";
 
 export default function PiedDePage() {
-return (
-
-
-<div     className="dark bg-black"> 
-<Footer container>
-  <Footer.Copyright
-    by="Quizine™"
-    href="#"
-    year={2023}
-  />
-  <Footer.LinkGroup>
-    <Footer.Link href="https://www.linkedin.com/in/emilie-di-palma-b8b63a264/">
-      Emilie
-    </Footer.Link>
-    <Footer.Link href="https://www.linkedin.com/in/briac-gauthier-lefeuvre-460004263/">
-      Briac
-    </Footer.Link>
-    <Footer.Link href="#">
-      Mounir
-    </Footer.Link>
-    <Footer.Link href="https://www.linkedin.com/in/yannchavret/">
-      Yann
-    </Footer.Link>
-    <Footer.Link href="https://chat.openai.com/">
-      Chat GPT
-    </Footer.Link>
-  </Footer.LinkGroup>
-</Footer>
-</div> )
+  return (
+    <div className="dark bg-black">
+      <Footer container>
+        <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+          <Footer.Copyright
+            by="Quizine™"
+            href="#"
+            year={2023}
+          />
+          <Footer.LinkGroup className="flex flex-wrap gap-x-4 gap-y-1">
+            <Footer.Link href="https://www.linkedin.com/in/emilie-di-palma-b8b63a264/">Emilie</Footer.Link>
+            <Footer.Link href="https://www.linkedin.com/in/briac-gauthier-lefeuvre-460004263/">Briac</Footer.Link>
+            <Footer.Link href="#">Mounir</Footer.Link>
+            <Footer.Link href="https://www.linkedin.com/in/yannchavret/">Yann</Footer.Link>
+            <Footer.Link href="https://chat.openai.com/">Chat GPT</Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+      </Footer>
+    </div>
+  )
 }
