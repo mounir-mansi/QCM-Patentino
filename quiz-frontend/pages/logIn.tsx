af5b53747cb28374e5e4d9c135c8514fc0eab1b0
mounir-mansi <mansimounir1@gmail.com>
2026-03-07 08:23:18 +0100

    style: fix quiz and login layout

--- quiz-frontend/pages/logIn.tsx
+++ quiz-frontend/pages/logIn.tsx
@@ -4,19 +4,15 @@ import PiedDePage from '@/components/Footer';
 import LogInForm from "@/components/LogInForm";
 import Menu from '@/components/Menu';
 
-
 export default function logIn() {
-    return (
-      <div className="">
-            <Menu/>
-            
-            <div className="container mx-auto flex flex-col items-center justify-center w-screen min-content-height">
-  
-            <LogInForm/>
-  
-            </div>
-            <AnimatedBackground/>
-            <PiedDePage/>
+  return (
+    <div>
+      <Menu />
+      <AnimatedBackground />
+      <div className="relative z-10 flex items-center justify-center min-content-height px-4">
+        <LogInForm />
       </div>
-    )
-  }
+      <PiedDePage />
+    </div>
+  )
+}
